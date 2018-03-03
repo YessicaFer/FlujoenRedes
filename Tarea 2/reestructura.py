a5fa7b4f0254de931482aa189910ae98dd88febc
YessicaFer <35753071+YessicaFer@users.noreply.github.com>
2018-03-02 19:36:03 -0600

    Add files via upload

--- Tarea 2/reestructura.py
+++ Tarea 2/reestructura.py
@@ -32,6 +32,7 @@ class GrafoYessica:
 
     def conectar(self, tamaño):
         for x in range(tamaño):
+            v=choice([1,2,3,4,5])
             self.e=random()*10
             f=updown(self.e)
             for i in range (tamaño-2):
@@ -39,25 +40,46 @@ class GrafoYessica:
                     if euclidiana((self.x[x],self.y[x],self.c[x],self.d[x]),(self.x[i],self.y[i],self.c[i],self.d[i]))< euclidiana((self.x[x], self.y[x], self.c[x], self.d[x]),(self.x[m],self.y[m],self.c[m],self.d[m])):
                         if euclidiana((self.x[x],self.y[x],self.c[x],self.d[x]),(self.x[i],self.y[i],self.c[m],self.d[m]))<0.5:
                             if self.c[x]==self.c[i]:
-                                aristas.append((self.x[x], self.y[x], self.x[i], self.y[i],f))
+                                aristas.append((self.x[x], self.y[x], self.x[i], self.y[i],f,v))
                     else:
                         if euclidiana((self.x[x],self.y[x],self.c[x],self.d[x]),(self.x[x],self.y[m],self.c[m],self.d[m]))<0.5:
                             if self.c[x]==self.c[m]:
-                                aristas.append((self.x[x], self.y[x], self.x[m], self.y[m],f))
+                                aristas.append((self.x[x], self.y[x], self.x[m], self.y[m],f,v))
 
         with open("tarea2.plot", "w") as archivo:
-            print("set term pdf", file=archivo)
-            print("set output 'grafica2.pdf'", file=archivo)
+            print("set term eps", file=archivo)
+            print("set output 'grafica2.eps'", file=archivo)
             print("set xrange [-.1:1.1]", file=archivo)
             print("set yrange [-.1:1.1]", file=archivo)
             print("set pointsize 1", file=archivo)
             print("set size square", file=archivo)
             print("set key off", file=archivo)
-            num = 1
-            for a in aristas:
-                (self.x[num], self.y[num], self.x[num+1], self.y[num+1], f) = a
-                print("set arrow {:d} from {:f}, {:f} to {:f}, {:f} head filled size 0.07,3 lt {:d}".format(num, self.x[num], self.y[num], self.x[num+1], self.y[num+1], f), file=archivo)
-                num += 1
+            if grafoorientado is 1:
+                if grafocapacitado is 1:
+                    num = 1
+                    for a in aristas:
+                        (self.x[num], self.y[num], self.x[num+1], self.y[num+1], f, v) = a
+                        print("set arrow {:d} from {:f}, {:f} to {:f}, {:f} head filled size 0.04,8 lt {:d} dashtype {:d}".format(num, self.x[num], self.y[num], self.x[num+1], self.y[num+1], f, v), file=archivo)
+                        num += 1
+                else:
+                    num = 1
+                    for a in aristas:
+                        (self.x[num], self.y[num], self.x[num+1], self.y[num+1], f, v) = a
+                        print("set arrow {:d} from {:f}, {:f} to {:f}, {:f} head filled size 0.04,7 lt {:d}".format(num, self.x[num], self.y[num], self.x[num+1], self.y[num+1], f, v), file=archivo)
+                        num += 1
+            else:
+                if grafocapacitado is 1:
+                    num = 1
+                    for a in aristas:
+                        (self.x[num], self.y[num], self.x[num+1], self.y[num+1], f, v) = a
+                        print("set arrow {:d} from {:f}, {:f} to {:f}, {:f} nohead lt {:d} dashtype {:d}".format(num, self.x[num], self.y[num], self.x[num+1], self.y[num+1], f, v), file=archivo)
+                        num += 1
+                else:
+                    num = 1
+                    for a in aristas:
+                        (self.x[num], self.y[num], self.x[num+1], self.y[num+1], f, v) = a
+                        print("set arrow {:d} from {:f}, {:f} to {:f}, {:f} nohead lw 1.5 lt {:d}".format(num, self.x[num], self.y[num], self.x[num+1], self.y[num+1], f, v), file=archivo)
+                        num += 1
             print("show arrow", file=archivo)
             print("plot 'prueba1.dat' using 1:2:3:4 with points pt var lc palette var", file=archivo)
             print("quit()", file=archivo)
@@ -65,8 +87,8 @@ class GrafoYessica:
             
 n=20
 aristas=[]
-grafoorientado=1      #para que sea orientado grafoorientado=1/ de lo contrario 2
-grafocapacitado=1     #para que sea capacitado grafocapacitado=1/ de lo contrario 2
+grafoorientado=0      #para que sea orientado grafoorientado=1/ de lo contrario 2
+grafocapacitado=0     #para que sea capacitado grafocapacitado=1/ de lo contrario 2
 #si grafoorientado=2 y grafocapacitado=2 se forma un grafo simple
 G=GrafoYessica()
 G.nodoscrear(n)
