64def5e65e8f37a3d1e6ca3a546319e3477860fb
YessicaFer <35753071+YessicaFer@users.noreply.github.com>
2018-04-06 11:06:31 -0500

    Completo
    Sin reporte(falta realizarlo)

---
+++ Tarea 3/tarea3.R
@@ -0,0 +1,29 @@
+library(readr)
+library(prob)
+Dirigido <- read.csv("tDirigidoFT.csv", header=FALSE)
+NDirigido <- read.csv("tNoDirigidoFF.csv", header=FALSE)
+
+#El número 11 en los datos corresponde a la cantidad de tamaños diferentes de grafos y el 6 a las repeticiones de cada tamaño
+
+TDirigido <- data.frame()
+for (i in 1:11){
+  TDirigido <- rbind(TDirigido, Dirigido[((6*i)-5):(6*i),])
+}
+print(shapiro.test(TDirigido[,1])) #No es normal
+pdf("TDirigidoFT.pdf")
+boxplot(t(TDirigido), main="Tiempo vs N", xlab=c("Número de nodos"), ylab=c("Tiempo (segundos)"), plot=TRUE)
+par(new=TRUE, pty="m", xaxt="n", yaxt="n")
+plot(seq(1:11)^2, type="l" , xlab="", ylab="", col="blue", lwd=2)
+legend("topleft",col=c("Blue"), legend =c("Función Cuadratica"), lwd=3, bty = "n")
+
+TNoDirigido <- data.frame()
+for (i in 1:11){
+  TNoDirigido <- rbind(TNoDirigido, NDirigido[((6*i)-5):(6*i),])
+}
+print(shapiro.test(TNoDirigido[,1])) #No es normal
+pdf("TNoDirigidoFF.pdf")
+boxplot(t(TNoDirigido), main="Tiempo vs N", xlab=c("Número de nodos"), ylab=c("Tiempo (segundos)"), plot=TRUE)
+par(new=TRUE, pty="m", xaxt="n", yaxt="n")
+plot(seq(1:11)^2, type="l" , xlab="", ylab="", col="red", lwd=2)
+legend("topleft",col=c("red"), legend =c("Función Cuadratica"), lwd=3, bty = "n")
+graphics.off()
